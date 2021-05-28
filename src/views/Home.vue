18d88a9b83554355f4d70eca0311610211269ed7
lionatic <ahmadhakim404@gmail.com>
2021-05-28 15:01:47 +0700

    get data from firestore

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,28 @@
 <template>
- <h1>Home</h1>
+ <div>
+     <h1>Home</h1>
+    <p>Welcome {{ user }}</p>
+ </div>
 </template>
 
 <script>
+import db from '../config/db.js'
+
+export default {
+    name: 'home',
+    data: function() {
+        return {
+            user: null
+        }
+    },
+    mounted() {
+        db.collection('users')
+        .doc('05ZRQklzKJRCZImrZ7tL')
+        .get()
+        .then(snapshot => {
+            this.user = snapshot.data().name
+        })
+    }
+}
 
 </script>

0f8c1bafd2c125a685d74be57bfbcd9db4fbbbe0
lionatic <ahmadhakim404@gmail.com>
2021-05-28 15:15:05 +0700

    using props and modify home navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="app">
-    <Navigation/>
-    <router-view />
+    <Navigation :user="user"/>
+    <router-view :user="user"/>
   </div>
 </template>
 
 <script>
+import db from './config/db.js'
 import Navigation from '@/components/Navigation'
+
 export default {
+  name: 'App',
   components: {
     Navigation
-  }
+  },
+  data: function() {
+        return {
+            user: null
+        }
+    },
+    mounted() {
+        db.collection('users')
+        .doc('05ZRQklzKJRCZImrZ7tL')
+        .get()
+        .then(snapshot => {
+            this.user = snapshot.data().name
+        })
+    }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,30 @@
 <template>
- <div>
-     <h1>Home</h1>
-    <p>Welcome {{ user }}</p>
+ <div class="mt-3">
+     <div class="text-center">
+         Welcome <span class="font-weight-bold text-info">{{ user }}</span>
+     </div>
+     <div class="container text-center">
+         <div class="row justify-content-center">
+             <div class="col-10 col-md-10 col-lg-8 col-xl-7">
+                 <h4 class="display-4 text-primary mb-2">Vue Meet</h4>
+                 <p class="lead">
+                     This app use firebase and webRTC for real time communication and create multiple video chat between users.
+                 </p>
+                 <router-link class="btn btn-outline-primary mr-2" to="/login">
+                     Login
+                 </router-link>
+             </div>
+         </div>
+     </div>
  </div>
 </template>
 
 <script>
-import db from '../config/db.js'
-
 export default {
-    name: 'home',
-    data: function() {
-        return {
-            user: null
-        }
-    },
-    mounted() {
-        db.collection('users')
-        .doc('05ZRQklzKJRCZImrZ7tL')
-        .get()
-        .then(snapshot => {
-            this.user = snapshot.data().name
-        })
-    }
+    props: [
+        'user'
+    ],
+    name: 'Home'
 }
 
 </script>
